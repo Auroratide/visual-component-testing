8a36e05f70f1881bbb6d2e9636f9ee4ce4b1a8ad
Timothy Foster <tf.auroratide@gmail.com>
2024-06-20 15:45:50 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+	return (
+		<main>
+			<h1>Visual Component Testing</h1>
+		</main>
+	)
+}
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		tsconfigPaths(),
+		react(),
+	],
+	server: {
+		port: 3000,
+	},
+	test: {},
+})
